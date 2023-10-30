9408c7ee9fe7d9a22aad0409b5be2f6384edfc4d
AGulev <me@agulev.com>
2023-10-30 19:47:29 +0100

    initial ios

---
+++ extension-yandex-ads/src/extension_ios.mm
@@ -0,0 +1,21 @@
+#if defined(DM_PLATFORM_IOS)
+
+#include "extension_private.h"
+#include "extension_callback_private.h"
+
+namespace dmYandexAds
+{
+
+void Initialize_Ext() {
+}
+
+void ActivateApp() {
+}
+
+void Initialize() {
+    
+}
+
+}
+
+#endif
